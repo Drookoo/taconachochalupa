84ecfeb19807a5be190ccac7234177545ff171cb
root <root@mx.ccdc.local>
2018-11-09 14:20:18 -0500

    moved iredmail elsewhere

--- iRedMail-0.9.8/samples/dovecot/dovecot.sieve
+++
@@ -1,7 +0,0 @@
-require ["fileinto"];
-
-# rule:[Move Spam to Junk Folder]
-if header :is "X-Spam-Flag" "YES"
-{
-    fileinto "Junk";
-}
--- iRedMail-0.9.8/samples/mysql/sql/delete_anonymous_user.sql
+++
@@ -1,7 +0,0 @@
--- Delete anonymouse user.
-USE mysql;
-
-DELETE FROM user WHERE User='';
-DELETE FROM db WHERE User='';
-
-FLUSH PRIVILEGES;
