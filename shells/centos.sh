8ce47cf617d427f0df63360131cffbc91b740f1b
Drew Ku <andrewku123@gmail.com>
2018-11-18 02:35:10 -0500

    add centos.sh stuff

--- shells/centos.sh
+++ shells/centos.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
+
 #list upgradeable patches 
 yum check-update
 
 #list past upgrades, saves to file 
 
+#hardening
 
+#Increase the delay time between login prompts (10sec)
+sed -i "s/delay=[[:digit:]]\+/delay=10000000/" /etc/pam.d/login 
+
+#prevent root-owned files from accidentally becoming accessible to non priviledged users 
+usermod -g 0 root 
 
