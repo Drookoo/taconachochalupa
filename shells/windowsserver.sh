d54ffb8832cf29c353214ec2868d04050c5b5f91
Drew Ku <andrewku123@gmail.com>
2018-11-18 22:23:27 -0500

    commits

--- shells/windowsserver.sh
+++ shells/windowsserver.sh
@@ -1,2 +1,13 @@
+#Must install Choco (type this out manually) through ps
+#Run 'Get-ExecutionPolicy' 
+#if "Restricted", then run 'Set-ExecutionPolicy' or 'Set-ExecutionPolicy Bypass -Scope Process'
+#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#verify using 'choco'
+
+#install git
+#choco install -y git
+
 #show installed updates 
 Get-Hotfix
+
+
