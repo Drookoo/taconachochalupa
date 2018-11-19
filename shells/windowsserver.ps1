354a1ad37f8917fb6beed88684a03319039394d7
Drew Ku <andrewku123@gmail.com>
2018-11-19 17:51:18 -0500

    windows stuff

--- shells/windowsserver.ps1
+++ shells/windowsserver.ps1
@@ -8,52 +8,11 @@
 #choco install -y git
 #$env:Path += ";C:\Program Files\git\cmd"
 
-#show installed updates 
-Get-Hotfix
+#Get Installed Updates 
+$Session = New-Object -ComObject "Microsoft.Update.Session"
 
-#Show number of updates available
-$computername = $env:COMPUTERNAME
-$updatesession =  [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$Computername))
-$searchresult = $updatesearcher.Search("IsInstalled=0")  # 0 = NotInstalled | 1 = Installed
-$searchresult.Updates.Count 
-
-$Updates = If ($searchresult.Updates.Count  -gt 0) {
-	$count  = $searchresult.Updates.Count
-	Write-Verbose  "Found $Count update\s!"
-	For ($i=0; $i -lt $Count; $i++) {
-	$Update  = $searchresult.Updates.Item($i)
-	[pscustomobject]@{
-	Title =  $Update.Title
-	KB =  $($Update.KBArticleIDs)
-	SecurityBulletin = $($Update.SecurityBulletinIDs)
-	MsrcSeverity = $Update.MsrcSeverity
-	IsDownloaded = $Update.IsDownloaded
-	Url =  $Update.MoreInfoUrls
-	Categories =  ($Update.Categories  | Select-Object  -ExpandProperty Name)
-	BundledUpdates = @($Update.BundledUpdates)|ForEach{
-
-  [pscustomobject]@{
-
-  Title = $_.Title
-
-  DownloadUrl = @($_.DownloadContents).DownloadUrl
-
-  }
- }
-
-  }      
-
-  }
-
-  } 	
-
-#Dont know if this will run code 
-$IsDownloaded = $Updates|group IsDownloaded
-
-#Index 1 is downloaded updates 
-$IsDownloaded[1].Group  
-
-$IsDownloaded[1].Group  | Export-Csv -NoTypeInformation  -Path DownloadedUpdates.csv
-
+$Searcher = $Session.CreateUpdateSearcher()
+$historyCount = $Searcher.GetTotalHistoryCount()
+$Searcher.QueryHistory(0, $historyCount) | Select-Object Date,@{name="Operation"; expression={switch($_.operation){1 {"Installation"}; 2 {"Uninstallation"}; 3 {"Other"}}}}, @{name="Status"; expression={switch($_.resultcode){1 {"In Progress"}; 2 {"Succeeded"}; 3 {"Succeeded With Errors"};4 {"Failed"}; 5 {"Aborted"} }}}, Title, Description | Export-Csv -NoType "$Env:userprofile\Desktop\Windows Updates.csv"
 
 
