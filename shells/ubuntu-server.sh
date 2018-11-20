58eb0eb28c9e01c1182918b1082df95038e76bc1
Drew Ku <andrewku123@gmail.com>
2018-11-20 14:32:12 -0500

    yolo commit

--- shells/ubuntu-server.sh
+++ shells/ubuntu-server.sh
@@ -6,6 +6,6 @@
 sudo apt install nmap 
 sudo apt install xsltproc 
 
-sudo nmap -A 172.20.241.0/24 -oX internal.xml && sudo xsltproc internal.xml -o dmz.html && sudo rm -rf internal.xml
+sudo nmap -A 172.20.241.0/24 -oX internal.xml && sudo xsltproc internal.xml -o internal.html && sudo rm -rf internal.xml
 
 sudo nmap -A 172.20.240.0/24 -oX dmz.xml && sudo xsltproc dmz.xml -o dmz.html &&sudo rm dmz.xml 
