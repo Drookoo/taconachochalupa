42b31f8d395fc469273d5a90f354e86eaf7d6369
root <andrewku123@gmail.com>
2018-12-03 22:27:59 +0000

    try me

--- shells/ubuntu-rename.sh
+++ shells/ubuntu-rename.sh
@@ -9,8 +9,8 @@
 sudo apt install nmap 
 sudo apt install xsltproc 
 
-sudo nmap -v -A -T4 172.20.241.0/24 -oX ../internal.xml && sudo xsltproc ../internal.xml -o ../internal.html
+sudo nmap -v -A -T4 172.16.5.0/24 -oX ../internal.xml && sudo xsltproc ../internal.xml -o ../internal.html
 
-sudo nmap -v -A -T4 172.20.240.0/24 -oX ../dmz.xml && sudo xsltproc ../dmz.xml -o ../dmz.html
+sudo nmap -v -A -T4 10.5.11.0/28 -oX ../dmz.xml && sudo xsltproc ../dmz.xml -o ../dmz.html
 
 sudo git add -A && sudo git commit -m "ubuntu server nmap scans" && sudo git push origin master 
