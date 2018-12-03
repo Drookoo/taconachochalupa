78cbba1b709a73117179204e6b8c4bb67cf9154c
Drew Ku <andrewku123@gmail.com>
2018-12-03 18:23:45 -0500

    a lot of changes

---
+++ shells/ubuntu-nessus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#Nessus installer for internal ubuntu machine, Script #2 
+
+
+cd ..
+cd installs 
+sudo dpkg -i Nessus-ubuntu.deb
+sudo /etc/init.d/nessusd start 
+sudo /opt/nessus/sbin/nessuscli fetch --register 7261-CF0A-6E86-1E8E-3064
+sudo /opt/nessus/sbin/nessuscli update
+
+
+
