6a3559560b98f075152a58a6451b6fb2a986cee7
Drew Ku <andrewku123@gmail.com>
2018-12-03 18:58:35 -0500

    hello

--- shells/ubuntu-nessus.sh
+++ shells/ubuntu-nessus.sh
@@ -9,6 +9,5 @@ sudo dpkg -i Nessus-ubuntu.deb
 sudo /etc/init.d/nessusd start 
 sudo /opt/nessus/sbin/nessuscli fetch --register 7261-CF0A-6E86-1E8E-3064
 sudo /opt/nessus/sbin/nessuscli update
-
-
+sudo systemctl restart nessusd 
 
