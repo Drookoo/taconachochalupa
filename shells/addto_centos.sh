3c636861a40f3066af8280cb835527adbf328f4d
Drew Ku <andrewku123@gmail.com>
2018-12-12 19:46:05 -0500

    commit

--- shells/addto_centos.sh
+++ shells/addto_centos.sh
@@ -20,11 +20,57 @@ disable_postfix() {
 		systemctl stop postfix
 		systemctl disable postfix
 	}
+	
+kernel_tuning() {
+    sysctl kernel.randomize_va_space=1
+    
+    # Enable IP spoofing protection
+    sysctl net.ipv4.conf.all.rp_filter=1
+
+    # Disable IP source routing
+    sysctl net.ipv4.conf.all.accept_source_route=0
+    
+    # Ignoring broadcasts request
+    sysctl net.ipv4.icmp_echo_ignore_broadcasts=1
+    sysctl net.ipv4.icmp_ignore_bogus_error_messages=1
+    
+    # Make sure spoofed packets get logged
+    sysctl net.ipv4.conf.all.log_martians=1
+    sysctl net.ipv4.conf.default.log_martians=1
+
+    # Disable ICMP routing redirects
+    sysctl -w net.ipv4.conf.all.accept_redirects=0
+    sysctl -w net.ipv6.conf.all.accept_redirects=0
+    sysctl -w net.ipv4.conf.all.send_redirects=0
+    sysctl -w net.ipv6.conf.all.send_redirects=0
+
+    # Disables the magic-sysrq key
+    sysctl kernel.sysrq=0
+    
+    # Turn off the tcp_timestamps
+    sysctl net.ipv4.tcp_timestamps=0
+
+    # Enable TCP SYN Cookie Protection
+    sysctl net.ipv4.tcp_syncookies=1
+
+    # Enable bad error message Protection
+    sysctl net.ipv4.icmp_ignore_bogus_error_responses=1
+    
+    # RELOAD WITH NEW SETTINGS
+    sysctl -p
+}
+
+disable_ipv6() {
+    sysctl -w net.ipv6.conf.default.disable_ipv6=1
+    sysctl -w net.ipv6.conf.all.disable_ipv6=1
+}
 
 main() {
 	    sys_upgrades
 		permission_narrowing
 		disable_postfix
+		kernel_tuning
+		disable_ipv6
 	}
 
 main "$@"
