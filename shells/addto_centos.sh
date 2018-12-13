6706743de55dd39883d6d1f9c7bf70198d9a3156
Drew Ku <andrewku123@gmail.com>
2018-12-12 19:44:25 -0500

    commit

--- shells/addto_centos.sh
+++ shells/addto_centos.sh
@@ -1,42 +1,33 @@
 sys_upgrades() {
 	    yum -y update
-	        yum -y upgrade
-		    yum -y autoremove
+	    yum -y upgrade
+		yum -y autoremove
 	    }
 
 permission_narrowing() {
 	    chmod 700 /root
-	        chmod 700 /var/log/audit
-		    chmod 740 /etc/rc.d/init.d/iptables
-		        chmod 740 /sbin/iptables
-			    chmod  700 /etc/skel
-			        chmod 600 /etc/rsyslog.conf
-				    chmod 640 /etc/security/access.conf
-				        chmod 600 /etc/sysctl.conf
+	    chmod 700 /var/log/audit
+		chmod 740 /etc/sysconfig/iptables-config
+		chmod 740 /etc/sysconfig/firewalld
+		chmod 740 /sbin/iptables
+		chmod  700 /etc/skel
+		chmod 600 /etc/rsyslog.conf
+		chmod 640 /etc/security/access.conf
+		chmod 600 /etc/sysctl.conf
 				}
 
-disable_avahi(){
-	    systemctl stop avahi-daemon.socket avahi-daemon.service
-	        systemctl disable avahi-daemon.socket avahi-daemon.service
+disable_postfix() {
+		systemctl stop postfix
+		systemctl disable postfix
 	}
-
-
-
-
-
-	disable_postfix() {
-		    systemctl stop postfix
-		        systemctl disable postfix
-		}
 
 main() {
 	    sys_upgrades
-				        permission_narrowing
-					    disable_avahi
-					        disable_postfix
-					    }
+		permission_narrowing
+		disable_postfix
+	}
 
-					    main "$@"
+main "$@"
 
 
 
