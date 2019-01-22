92ca731ad9873c88fc6739209e5611cd90931b9c
Drew Ku <andrewku123@gmail.com>
2019-01-22 13:34:23 -0500

    hello

--- shells/ubuntu_bonus_installs.sh
+++ shells/ubuntu_bonus_installs.sh
@@ -20,6 +20,32 @@ sudo docker pull vuls/gost
 # https://vuls.io/docs/en/tutorial-local-scan.html
 # google tutorial for docker vuls 
 
+#################################
+#OpenVas 
+sudo add-apt-repository ppa:mrazavi/openvas
+sudo apt-get update -y
+sudo apt-get install sqlite3 openvas9 -y
+#hit yes 
+sudo apt-get install texlive-latex-extra --no-install-recommends -y
+sudo apt-get install texlive-fonts-recommended --no-install-recommends -y
+sudo apt-get install libopenvas9-dev
+
+sudo greenbone-nvt-sync 
+sudo greenbone-scapdata-sync 
+sudo greenbone-certdata-sync 
+
+sudo systemctl restart openvas-scanner
+sudo systemctl restart openvas-manager
+sudo systemctl restart openvas-gsa
+sudo openvasmd --rebuild --progress --verbose 
+
+#check for install errors 
+# sudo wget --no-check-certificate https://svn.wald.intevation.org/svn/openvas/branches/tools-attic/openvas-check-setup -P /usr/local/bin/
+# sudo chmod +x /usr/local/bin/openvas-check-setup
+
+# openvas-check-setup --v9
+
+
 
 
 
