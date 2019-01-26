6973dcffdac796d304d21761b75347ce61566ec6
Ku <ak55605p@pace.edu>
2019-01-26 08:37:02 -0500

    changeme

--- shells/superbuntu-internal.sh
+++ shells/superbuntu-internal.sh
@@ -43,7 +43,7 @@ sudo systemctl restart nessusd
 cd ..
 #######################################################
 
-sudo nmap -v -A -T4 172.16.5.0/24 -oX ../internal.xml && sudo xsltproc ../internal.xml -o ../internal.html
+sudo nmap -v -A -T4 172.16.1.0/24 -oX ../internal.xml && sudo xsltproc ../internal.xml -o ../internal.html
 sudo nmap -v -A -T4 10.5.11.0/28 -oX ../dmz.xml && sudo xsltproc ../dmz.xml -o ../dmz.html
 
 sudo git config --global user.email "andrewku123@gmail.com"
